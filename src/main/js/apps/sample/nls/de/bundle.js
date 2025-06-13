91b69514c207cc30a1eb799ec69e50a5dc02cd9c
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2025-06-13 13:31:32 +0200

    Update sample app

--- src/main/js/apps/sample/nls/de/bundle.js
+++ src/main/js/apps/sample/nls/de/bundle.js
@@ -14,17 +14,85 @@
  * limitations under the License.
  */
 module.exports = {
-    apptitle: "map.apps 4 Selection Actions Sample",
+    apptitle: "Selection Actions Sample",
     map: {
-        esrisites: {
-            title: "Standorte der Esri Deutschland Gruppe",
-            popup: {
-                address: "Adresse",
-                contact: "Kontakt",
-                tel: "Telefon:",
-                mail: "Email:",
-                link: "Internet:"
+        koeln1: {
+            title: "Basisdaten",
+            districts: {
+                title: "Stadtviertel",
+                text: "Das K\xF6lner Stadtviertel <b>{STV_NAME}</b>."
+            },
+            boroughs: {
+                title: "Stadtteile",
+                text: "Der Stadtteil <b>{NAME}</b> liegt im K\xF6lner Stadtbezirk {STADTBEZIR}."
+            },
+            precints: {
+                title: "Stadtbezirke",
+                text: "Der K\xF6lner Stadtbezirk <b>{NAME}</b>."
             }
+        },
+        koeln2: {
+            title: "Bildung und Kultur",
+            description: "Liste aller Bibliotheken, Museen und Schulen in Köln.",
+            libraries: {
+                title: "Bibliotheken"
+            },
+            museums: {
+                title: "Museen",
+                text: "Das Museum <b>{NAME}</b> liegt im K\xF6lner Stadtteil {STADTTEIL}."
+            },
+            schools: {
+                title: "Schulen",
+                text: ""
+            }
+        },
+        koeln3: {
+            title: "Freizeit",
+            sights: {
+                title: "Sehensw\xFCrdigkeiten",
+                titleSingle: "Sehensw\xFCrdigkeit",
+                text: "Die Sehensw\xFCrdigkeit <b>{NAME}</b> liegt im K\xF6lner Stadtteil {STADTTEIL}."
+            },
+            playgrounds: {
+                title: "Spiel- und Sportpl\xE4tze",
+                text: "<b>{Spielplatzname}</b> liegt im K\xF6lner Stadtteil {Stadtteil}.",
+                baskets: "Basketballk\xF6rbe",
+                goals: "Fussballtore",
+                tables: "Tischtennis Tische",
+                walls: "Torwand",
+                skate: "Skaten",
+                misc: "Sonstiges"
+            },
+            places: {
+                title: "Veranstaltungsorte",
+                titleSingle: "Veranstaltungsort",
+                text: "<b>{NAME}</b> ist ein {expression/carrier} Veranstaltungsort."
+            }
+        },
+        basemaps: {
+            gray: "Stra\xDFenkarte (grau)",
+            streets: "Stra\xDFenkarte",
+            topo: "Topographische Karte",
+            hybrid: "Luftbild (hybrid)"
         }
+    },
+    tools: {
+        drawerLeft: "Werkzeuge",
+        measuring: "Messwerkzeuge"
+    },
+    common: {
+        number: "Nummer",
+        area: "Fl\xE4che [ha]",
+        totalArea: "Anteil an Gesamtfl\xE4che [%]",
+        name: "Name",
+        provider: "Tr\xE4ger",
+        address: "Adresse",
+        furtherinfo: "Weitere Informationen",
+        precint: "Stadtbezirk",
+        district: "Stadtviertel",
+        private: "private",
+        municipal: "st\xE4dtischer",
+        zip: "PLZ",
+        type: "Art"
     }
 };
